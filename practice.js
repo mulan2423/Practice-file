653c71376e50db6e7bd769827a8c39c6b1e230bd
Adelaide Severe <adelaidesevere@Adelaides-MacBook-Pro.local>
2023-01-07 14:58:17 -0500

    start 90s website

---
+++ practice.js
@@ -0,0 +1,32 @@
+// practice.js
+const MyInfo= {
+    firstName: "Addy",
+    LastName: "Severe",
+    CurrentDate: "12 20,2022",
+
+};
+console.log (MyInfo)
+const people = [
+    {
+      name: 'Billy',
+      age: 20,
+      interests: {
+        sports: ['golf', 'soccer', 'baseball'],
+        food: ['bacon', 'pineapple', 'sushi']
+      }
+    }, {
+      name: 'Jane',
+      age: 22,
+    }, {
+      name: 'Susie',
+      age: 24,
+    }
+  ];
+  
+  let n =1;
+  while (n<=10) {
+    console.log (n);
+    n = n + 1;
+  }  
+  const dayOfWeek = ['1, 2, 3']
+  console.log(dayOfWeek)
